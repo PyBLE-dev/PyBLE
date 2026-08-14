ca5c113ba965620bc684cafcac4b20056945410f
Viwat Vchirawongkwin <viwat.v@chula.ac.th>
2026-08-14 13:42:41 +0700

    [red] Reject shortest GNU ld map option
    Signed-off-by: Viwat Vchirawongkwin <viwat.v@chula.ac.th>

--- tests/firmware_tests/host/test_release_ninja_link_evidence.py
+++ tests/firmware_tests/host/test_release_ninja_link_evidence.py
@@ -374,6 +374,19 @@ class NinjaLinkEvidenceTests(unittest.TestCase):
                 with self.assertRaises(RELEASE.ReleaseError):
                     self.observe()
 
+    def test_gnu_ld_short_map_option_is_rejected(self):
+        for flags in (
+            " -Wl,--M=attacker.map",
+            " -Wl,--M,attacker.map",
+            " --for-linker=--M=attacker.map",
+            " --for-linker --M --for-linker attacker.map",
+            " -Xlinker --M -Xlinker attacker.map",
+        ):
+            with self.subTest(flags=flags):
+                self._write_ninja(flags=flags)
+                with self.assertRaises(RELEASE.ReleaseError):
+                    self.observe()
+
     def test_linker_map_output_must_match_the_observed_map(self):
         build_ninja = self.role_build / "build.ninja"
         attacker_map = self.role_build / "attacker.map"
